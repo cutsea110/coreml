7616bfeb4ca2b2f6fc89222952bb50d233e94144
cutsea110 <cutsea110@gmail.com>
2026-07-05 08:49:57 +0900

    implement addOne

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,13 @@
 module Main (main) where
 
+import Eval (eval)
+import TM   (Tape, S(..), p)
+
+t :: Tape
+t = ([I, I, I], I, [])
+
+r :: Tape
+r = eval p t
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = print (t, r)
--- src/TM.hs
+++ src/TM.hs
@@ -1,2 +1,24 @@
 module TM where
 
+-- R: right
+-- L: left
+data D = R | L deriving (Show, Eq)
+-- B: blank
+-- I: 1
+-- O: 0
+data S = B | I | O deriving (Show, Eq)
+-- M: move
+-- H: halt
+data Q = M | H deriving (Show, Eq)
+
+type Delta = [((Q, S), (Q, S, D))]
+
+type Program = (Q, Delta)
+
+type Tape = ([S], S, [S])
+
+p :: Program
+p = (M, [ ((M, I), (M, O, L))
+        , ((M, O), (M, I, L))
+        , ((M, B), (H, I, L))
+        ])
