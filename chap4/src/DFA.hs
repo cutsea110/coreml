98ee9b1db1e9a25dbda13afbe9888d904cb44cf0
cutsea110 <cutsea110@gmail.com>
2026-09-06 09:11:25 +0900

    start to implement dfa

--- chap4/src/DFA.hs
+++ chap4/src/DFA.hs
@@ -1,1 +1,43 @@
+{-# LANGUAGE DuplicateRecordFields #-}
 module DFA where
+
+import Data.List (nub)
+
+type S = String
+
+epsilon :: S
+epsilon = ""
+
+type Q = Int
+
+type Delta = [(Q, (S, [Q]))]
+
+data NFA = NFA { q     :: [Q]
+               , s     :: [S]
+               , delta :: Delta
+               , q0    :: Q
+               , f     :: [Q]
+               }
+         deriving (Show, Eq)
+
+type State = [Q]
+
+type Delta' = [(State, [(S, State)])]
+
+data DFA = DFA { q      :: [State]
+               , s      :: [S]
+               , delta' :: Delta'
+               , q0     :: State
+               , f      :: [State]
+               }
+         deriving (Show, Eq)
+
+epsilonCl :: Delta -> [Q] -> [Q]
+epsilonCl d ps = flatten [ qs
+                         | p <- ps
+                         , qs <- [ qs'
+                                 | (p', (symbol, qs')) <- d
+                                 , p' == p && symbol == epsilon
+                                 ]
+                         ]
+  where flatten = nub . concat
