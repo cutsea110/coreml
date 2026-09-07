a37c9e8a4fd74ecd96d72bdbd23bbbb81a753b58
cutsea110 <cutsea110@gmail.com>
2026-09-07 11:25:40 +0900

    cabal test for subdirectories of target srcDir

--- compiler/test/Doctest.hs
+++ compiler/test/Doctest.hs
@@ -1,16 +1,24 @@
 module Main (main) where
 
 import Control.Monad (forM)
-import System.Directory (getDirectoryContents)
+import System.Directory (doesDirectoryExist, listDirectory)
 import System.FilePath (takeExtension, (</>))
 import Test.DocTest (doctest)
 
 targetDir :: [FilePath]
 targetDir = ["lex","main"]
 
+findHsFiles :: FilePath -> IO [FilePath]
+findHsFiles dir = do
+  entries <- listDirectory dir
+  concat <$> forM entries (\entry -> do
+    let path = dir </> entry
+    isDir <- doesDirectoryExist path
+    if isDir
+      then findHsFiles path
+      else return [path | takeExtension path == ".hs"])
+
 main :: IO ()
 main = do
-  files <- concat <$> forM targetDir (\d -> do
-    dir <- getDirectoryContents d
-    return $ (d </>) <$> filter (\f -> takeExtension f == ".hs") dir)
+  files <- concat <$> forM targetDir findHsFiles
   doctest $ map ("-i" ++) targetDir ++ files
