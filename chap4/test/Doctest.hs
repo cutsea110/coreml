46f25d42b1a456f4b5906451c57cce39eb9397b9
cutsea110 <cutsea110@gmail.com>
2026-09-07 11:10:13 +0900

    test for all source dir

--- chap4/test/Doctest.hs
+++ chap4/test/Doctest.hs
@@ -1,9 +1,16 @@
 module Main (main) where
 
+import Control.Monad (forM)
+import System.Directory (getDirectoryContents)
+import System.FilePath (takeExtension, (</>))
 import Test.DocTest (doctest)
 
+targetDir :: [FilePath]
+targetDir = ["src", "app"]
+
 main :: IO ()
-main = doctest
-  [ "-isrc"
-  , "src/DFA.hs"
-  ]
+main = do
+  files <- concat <$> forM targetDir (\d -> do
+    dir <- getDirectoryContents d
+    return $ map (d </>) (filter (\f -> takeExtension f == ".hs") dir))
+  doctest $ map ("-i" ++) targetDir ++ files
--- compiler/test/Doctest.hs
+++ compiler/test/Doctest.hs
@@ -1,12 +1,16 @@
 module Main (main) where
 
+import Control.Monad (forM)
+import System.Directory (getDirectoryContents)
+import System.FilePath (takeExtension, (</>))
 import Test.DocTest (doctest)
 
+targetDir :: [FilePath]
+targetDir = ["lex","main"]
+
 main :: IO ()
-main = doctest
-  [ "-isrc"
-  , "lex/Lexer.hs"
-  , "lex/Token.hs"
-  , "lex/Parser.hs"
-  , "main/Top.hs"
-  ]
+main = do
+  files <- concat <$> forM targetDir (\d -> do
+    dir <- getDirectoryContents d
+    return $ map (d </>) (filter (\f -> takeExtension f == ".hs") dir))
+  doctest $ map ("-i" ++) targetDir ++ files
